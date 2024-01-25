a40397627b0201dd1b1c04bf8711230424d97041
Derek <adhoc607@gmail.com>
2024-01-24 23:58:56 -0500

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 import { MainArticle } from "./components/MainArticle";
 import { NewContainer } from "./components/NewContainer";
 import { Header } from "./components/Header";
+import { ArticlesContainer } from "./components/ArticlesContainer";
 
 function App() {
   return (
     <>
-      <main className="px-4 pt-6 pb-3 font-Inter lg:px-40 lg:pt-20" >
-        <Header/>
-      <div className="sm:flex sm:gap-8">
-  
-        <MainArticle />
-        <NewContainer />
-      </div>
-      
+      <main className="px-4 pt-6 pb-3 font-Inter lg:px-40 lg:pt-20">
+        <Header />
+        <div className="md:flex md:gap-8 md:mb-10">
+          <MainArticle />
+          <NewContainer />
+        </div>
+        <ArticlesContainer />
       </main>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import logo from '../assets/images/logo.svg'
-import { NavBar } from './Navbar'
+import { Navbar } from './Navbar'
 
 export const Header = () => {
   return (
     <header className='flex place-content-between items-center mb-14'>
       <img className='cursor-pointer' src={logo} alt="logo" />
-      <NavBar/>
+      <Navbar />
     </header>
   )
 }
